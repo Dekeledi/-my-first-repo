3945caf1abd3336ee3254c14bd8abdf75e193593
Dekiledi <radesanya2010@gmail.com>
2024-11-12 13:06:53 +0100

    my first commit

---
+++ dataTypes.js
@@ -0,0 +1,80 @@
+// alert("hello js class")
+
+console.log("welocme to my js class")
+
+
+
+// three key to hold data
+// var
+// let
+// const
+
+var name = "dekeledi";
+console.log(name);
+
+let age = 40;
+console.log("age");
+
+const email = "dekeledi@gmail.com";
+console.log(email);
+
+
+
+// datatypes
+// premitive datatype
+// non-premitive datatype
+
+
+// primitive
+// string
+let name2 = "dekeledi";
+// number
+// integer
+let age2 = 25;                             
+// float
+let myNumber = 0.94;
+// booolean
+let isMarried = true;
+// undefined
+let notAssigned;
+// null
+let isActive = null;
+
+// non-premitive
+// array
+let myArray = ["bag", "shoes", "hat", "jacket", "socks"];
+console.log(myArray[3][2]);
+
+//object
+let user={
+    name:"dekeledi",
+    age:20,
+    email:"rosemary@gmail.com",
+    isStudent:false
+};
+
+console.log(user)
+
+const user2 = {
+    id :1,
+    firstName: "john",
+    lastName: "Doe",
+    userName:"dekeledi",
+    email: "john.doe@example.com",
+    password:"unknowodinak123@34#$%^^",
+    phone: "+1234567890",
+    address:{
+        street:"123 Main St",
+        city:"New York",
+        state:"NY",
+        postalCode:"1000001",
+        country:"canada"
+    },
+
+    dateOfBirth:"1990-01-01",
+    isActive:true,
+    roles:["admin","user"],
+    lastLogin: "2024-10-14T10:30:00Z"
+};
+
+console.log(user2)
---
+++ operators.js
@@ -0,0 +1,115 @@
+// operators in js are special symbols that are used to perform operations on variables and values in our js system
+// arithmetic operators
+// + - * / % ++ --
+
+// addition +
+let sum = 10 + 20;
+console.log(sum);
+
+// subtraction -
+let difference = 2 - sum;
+console.log(difference);
+
+// multiplication *
+let product = sum * difference;
+console.log(product);
+
+// division /
+let quotient = product / 4;
+console.log(quotient);
+
+// modulus %
+let remainder = quotient % 3;
+console.log(remainder);
+
+
+// assignment operators
+// = += -= *= /= %=
+// the assignment operator is used to assign a value to a variable
+
+// assignment =
+let x = 5;
+console.log(x);
+
+// addition assignment +=
+ let y = 5;
+ y += 1;
+ console.log(y);
+
+// subtraction assignment  -=
+let z = 10;
+z -= y;
+console.log(z);
+
+// multiplication assignment *=
+// let Z = 20;
+// z *=  y;
+
+// division assignment /=
+// let Z = 20;
+// z /=  y;
+
+
+// comparison operators
+// == != > < >= <=
+// the comparison operators are used to compare two values and return a boolean value
+
+// equal to ==
+console.log(10 == "10");
+
+// strict equal to ===
+console.log(10 === "10");
+
+// not equal to !=
+console.log(10 != "10");
+let n = 10;
+let Y = "10";
+console.log(n != y);
+
+// srtict not equal to !==
+console.log(10 !== "10");
+let u = "dekeledi";
+let i = "10";
+console.log(u !== i);
+
+// greater than >
+console.log(10 > 5);
+
+// less than <
+console.log(10 < 5);
+let l = 12;
+let I = 10;
+let w =  l - i;
+console.log(x < w);
+
+
+// greater than or equal to >=
+console.log(10 >= 5);
+let U = 10;
+let v  = 5;
+console.log(u >= v);
+
+
+let o = v - 3
+
+console.log(n >= o);
+
+let a = 3;
+let b = 2;
+console.log(a >= b);
+
+
+
+
+
+
+
+// increment ++
+// let increment = 10;
+// increment++;
+// console.log(increment);
+
+// decrement --
+// let decrement = 10;
+// decrement--;
+// console.log(decrement);
